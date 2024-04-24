fad8d7d08de0536b32b7d03b8a802729e315c017
rsgamertv <Wersuser2@gmail.com>
2024-04-24 14:38:55 +0300

    commit

--- lib/chats/widgets/message_bubble_widget.dart
+++ lib/chats/widgets/message_bubble_widget.dart
@@ -1,7 +1,9 @@
 import 'package:authentication/core/styles/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
+import 'package:sizer/sizer.dart';
 
 import '../../authentication/models/user_model.dart';
 import '../../core/styles/text_styles.dart';
@@ -22,32 +24,25 @@ Widget message_bubble_widget(MessageModel message, context){
     child: Align(
       alignment: alignment,
       child: Container(
-        //constraints: BoxConstraints(maxWidth: size.width * 0.66),
+        constraints: BoxConstraints(maxWidth: size.width * 0.66),
         padding: EdgeInsets.all(8.0),
         margin: EdgeInsets.all(4.0),
         decoration: BoxDecoration(
           color: color,
           borderRadius: BorderRadius.circular(8),
         ),
-        child: Row(
-          children: [
-            Text(
-                message.message,
-                style: small_white()
-            ),
-            SizedBox(width: 10,),
-            Column(
-              children: [
-                SizedBox(height: 5,),
-                Text(
-                  message.sentAt.hour.toString() + ':' + (message.sentAt.minute.toString().length == 1 ? '0' + message.sentAt.minute.toString() : message.sentAt.minute.toString()),
-                  style: TextStyle(color: light_grey(), fontSize: 10),
-                  textAlign: TextAlign.end,
-                ),
-              ],
-            )
-          ],
-        )
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(child: Text(message.message, style: small_white(),)),
+          SizedBox(width: 1.5.w,),
+          Text(message.sentAt.hour.toString() + ':' + (message.sentAt.toString().length == 1 ?  '0${message.sentAt.minute}' : message.sentAt.minute.toString()), style: small_white(), textAlign: TextAlign.right,),
+            
+
+          
+        ],
+      ),
       ),
     ),
   );
